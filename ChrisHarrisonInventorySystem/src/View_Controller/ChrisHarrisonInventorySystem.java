6850590d0a4521c4bbea5068123b8f372ebdecf1
CKHarrison <hirohumi1882@gmail.com>
2019-11-21 22:52:17 -0700

    created addProductView and Controller. set up skeleton for modifyProductView and Controller

--- ChrisHarrisonInventorySystem/src/View_Controller/ChrisHarrisonInventorySystem.java
+++ ChrisHarrisonInventorySystem/src/View_Controller/ChrisHarrisonInventorySystem.java
@@ -19,7 +19,7 @@ public class ChrisHarrisonInventorySystem extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("ModifyPartView.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("AddProductView.fxml"));
         
         Scene scene = new Scene(root);
         
