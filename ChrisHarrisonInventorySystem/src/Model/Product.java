b4f2aa575182f2c9e4489e8ffb71c6e9c3be4921
CKHarrison <hirohumi1882@gmail.com>
2019-11-22 15:47:02 -0700

    Created Product Model

--- ChrisHarrisonInventorySystem/src/Model/Product.java
+++ ChrisHarrisonInventorySystem/src/Model/Product.java
@@ -19,7 +19,88 @@ public class Product extends Part {
     private int stock;
     private int min;
     private int max;
+
+    public Product(int id, String name, double price, int stock, int min, int max) {
+        super(id, name, price, stock, min, max);
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+        this.min = min;
+        this.max = max;
+    }
+
+    @Override
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    @Override
+    public void setMin(int min) {
+        this.min = min;
+    }
+
+    @Override
+    public void setMax(int max) {
+        this.max = max;
+    }
+
+    @Override
+    public int getId() {
+       return this.id;
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+      }
+
+    @Override
+    public double getPrice() {
+        return this.price;
+    }
+
+    @Override
+    public int getStock() {
+        return this.stock;
+    }
+
+    @Override
+    public int getMin() {
+        return this.min;
+    }
+
+    @Override
+    public int getMax() {
+        return this.max;
+    }
     
+    public void addAssociatedPart(Part part) {
+        associatedParts.add(part);
+    }
     
+    public void deleteAssociatedPart(Part associatedPart) {
+        associatedParts.remove(associatedPart);
+    }
+    
+    public ObservableList<Part> getAllAssociatedParts() {
+        return associatedParts;
+    }
+       
     
 }
