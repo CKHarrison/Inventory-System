bec194c23e2003817a66a7eb27f30030b5a20daf
CKHarrison <hirohumi1882@gmail.com>
2019-11-23 12:44:58 -0700

    finished subclass OutSourcedPart. Also went through and removed generic comments from netbeans to the model classes

--- ChrisHarrisonInventorySystem/src/Model/InHousePart.java
+++ ChrisHarrisonInventorySystem/src/Model/InHousePart.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Model;
 
 /**
@@ -14,72 +10,6 @@ public class InHousePart extends Part {
 
     public InHousePart(int machineId, int id, String name, double price, int stock, int min, int max) {
         super(id, name, price, stock, min, max);
-        this.machineId = machineId;
-    }
-
-    
-
-    @Override
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    @Override
-    public void setName(String name) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public void setPrice(double price) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public void setStock(int stock) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public void setMin(int min) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public void setMax(int max) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public int getId() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public String getName() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public double getPrice() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public int getStock() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public int getMin() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public int getMax() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-    
-    public void setMachineId(int machineId) {
         this.machineId = machineId;
     }
     
---
+++ ChrisHarrisonInventorySystem/src/Model/OutSourcedPart.java
@@ -0,0 +1,23 @@
+
+package Model;
+
+/**
+ *
+ * @author chris
+ */
+public class OutSourcedPart extends Part{
+    private String companyName;
+
+    public OutSourcedPart(String companyName, int id, String name, double price, int stock, int min, int max) {
+        super(id, name, price, stock, min, max);
+        this.companyName = companyName;
+    }    
+    
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+    
+    public String getCompanyName() {
+        return this.companyName;
+    }
+}
