1b3b5f8b08334571311c1167d8b40ef345a1ce9b
CKHarrison <hirohumi1882@gmail.com>
2019-11-23 22:31:42 -0700

    Realized why netbeans wasn't making the correct controller. Had modifyproductviewfxml connected to addproductview.fxml. fixed that bug. generated skeleton code for both views from scenebuilder.

--- ChrisHarrisonInventorySystem/src/View_Controller/AddProductViewController.java
+++ ChrisHarrisonInventorySystem/src/View_Controller/AddProductViewController.java
@@ -1,80 +1,81 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package View_Controller;
 
-import java.net.URL;
-import java.util.ResourceBundle;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 
-/**
- * FXML Controller class
- *
- * @author chris
- */
-public class AddProductViewController implements Initializable {
+public class AddProductViewController {
 
     @FXML
     private Button addProductSearchButton;
+
     @FXML
     private TextField addProductTextField;
+
     @FXML
     private TableView<?> addProductTableView;
+
     @FXML
     private TableColumn<?, ?> addPartIDTableColumn;
+
     @FXML
     private TableColumn<?, ?> addPartNameTableColumn;
+
     @FXML
     private TableColumn<?, ?> addPartInvLevelTableColumn;
+
     @FXML
     private TableColumn<?, ?> addPartPriceTableColumn;
+
     @FXML
     private Button addProductAddButton;
+
     @FXML
     private TableView<?> deleteProductTableView;
+
     @FXML
     private TableColumn<?, ?> deletePartTableColumn;
+
     @FXML
     private TableColumn<?, ?> deletePartNameTableColumn;
+
     @FXML
     private TableColumn<?, ?> deleteInvLevelTableColumn;
+
     @FXML
     private TableColumn<?, ?> deletePriceTableColumn;
+
     @FXML
     private Button deleteProductButton;
+
     @FXML
     private Button saveButton;
+
     @FXML
     private Button cancelButton;
+
     @FXML
     private Label IdLabel;
+
     @FXML
     private TextField addProductIDTextField;
+
     @FXML
     private TextField addProductNameTextField;
+
     @FXML
     private TextField addProductMaxTextField;
+
     @FXML
     private TextField addProductPriceTextField;
+
     @FXML
     private TextField addProductInvTextField;
+
     @FXML
     private TextField addProductMinTextField;
 
-    /**
-     * Initializes the controller class.
-     */
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
 }
--- ChrisHarrisonInventorySystem/src/View_Controller/ModifyProductViewController.java
+++ ChrisHarrisonInventorySystem/src/View_Controller/ModifyProductViewController.java
@@ -1,75 +1,81 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package View_Controller;
 
-import java.net.URL;
-import java.util.ResourceBundle;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 
-/**
- * FXML Controller class
- *
- * @author chris
- */
-public class ModifyProductViewController implements Initializable {
+public class ModifyProductViewController {
 
     @FXML
     private Button modifyProductSearchButton;
+
     @FXML
     private TextField modifyProductSearchTextField;
+
     @FXML
     private TableView<?> modifyProductAddTableView;
+
     @FXML
-    private TableColumn<?, ?> modifyProductPartIDTableColumn;
+    private TableColumn<?, ?> modifyProductAdddPartIDTableColumn;
+
     @FXML
-    private TableColumn<?, ?> modifyProductPartNameTableColumn;
+    private TableColumn<?, ?> modifyProductAddPartNameTableColumn;
+
     @FXML
-    private TableColumn<?, ?> modifyProductInvLevelTableColumn;
+    private TableColumn<?, ?> modifyProductAddInvLevelTableColumn;
+
     @FXML
-    private TableColumn<?, ?> modifyProductPriceTableColumn;
+    private TableColumn<?, ?> modifyProductAddPriceTableColumn;
+
     @FXML
     private Button modifyProductAddButton;
+
     @FXML
     private GridPane modifyProductDeletTableView;
+
     @FXML
     private TableView<?> modifyProductDeleteTableView;
+
     @FXML
-    private TableColumn<?, ?> modifyProductPartIdTableColumn;
+    private TableColumn<?, ?> modifyProductDeletePartIdTableColumn;
+
+    @FXML
+    private TableColumn<?, ?> modifyProductDeletePartNameTableColumn;
+
+    @FXML
+    private TableColumn<?, ?> modifyProductDeleteInvLevelTableColumn;
+
+    @FXML
+    private TableColumn<?, ?> modifyProductDeletePriceTableColumn;
+
     @FXML
     private Button modifyProductDeleteButton;
+
     @FXML
     private Button modifyProductSaveButton;
+
     @FXML
     private Button modifyProductCancelButton;
+
     @FXML
     private TextField modifyProductIdTextField;
+
     @FXML
     private TextField modifyProductNameTextField;
+
     @FXML
     private TextField modifyProductMaxTextField;
+
     @FXML
     private TextField modifyProductPriceTextField;
+
     @FXML
     private TextField modifyProductInvTextField;
+
     @FXML
     private TextField modifyProductMinTextField;
 
-    /**
-     * Initializes the controller class.
-     */
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
 }
